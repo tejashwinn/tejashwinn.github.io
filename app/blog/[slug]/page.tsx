c7f5132dd97c8f3e98d7aca0a01a50af08d2a98e
Tejashwin U <tejashwin.uday@gmail.com>
2025-10-01 15:38:26 +0530

    Working minimal application

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,18 +1,21 @@
-import fs from 'fs';
-import path from 'path';
-import { MDXRemote } from 'next-mdx-remote';
-import { serialize } from 'next-mdx-remote/serialize';
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter"; // for frontmatter metadata
+import dynamic from "next/dynamic";
 
-export default async function BlogPost({ params }: { params: { slug: string } }) {
-  const blog = await params.slug;
-  const filePath = path.join(process.cwd(), 'content/blog', `${blog}.mdx`);
-  const source = fs.readFileSync(filePath, 'utf-8');
-  const mdxSource = await serialize(source);
+export default async function BlogPost(props: { params: Promise<{ slug: string }> }) {
+    const params = await props.params;
+
+  const filePath = path.join(process.cwd(), "content/blog", `${params.slug}.mdx`);
+  const fileContent = fs.readFileSync(filePath, "utf-8");
+  const { content } = matter(fileContent);
+
+  // Import MDX file directly as a component
+  const Post = dynamic(() => import(`@/content/blog/${params.slug}.mdx`));
 
   return (
-    <article className="prose lg:prose-xl">
-      <MDXRemote {...mdxSource} 
-      />
+    <article className="prose lg:prose-xl mx-auto py-10">
+      <Post />
     </article>
   );
 }
